bc50af4e96cbc111f481f453a2912abca0af08b2
App-Ju <appjuice@mail.ru>
2022-05-30 17:21:15 +0700

    added enum

---
+++ src/typescript/enums.ts
@@ -0,0 +1,16 @@
+export enum tournamentType {
+  SingleElimination = 1,
+  DoubleElimination,
+  RoundRobin,
+}
+
+export enum howManyTimes {
+  One = 1,
+  Two,
+  Three,
+}
+
+export enum gridSize {
+  list = 1,
+  number,
+}
