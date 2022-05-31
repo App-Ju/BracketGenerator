d2e38bc49ed17ed513e1c114a05ea990909c2a8f
App-Ju <appjuice@mail.ru>
2022-05-31 15:37:13 +0700

    upd: Settings model

--- src/models/Settings.ts
+++ src/models/Settings.ts
@@ -1,27 +1,30 @@
-import { tournamentType, howManyTimes, bracketSize } from "@/typescript/enums";
+import { tournamentType, howManyGames, bracketSize } from "@/typescript/enums";
 
 export default class Settings {
   name: string;
-  type: tournamentType = tournamentType.SingleElimination;
-  participantNumber: bracketSize = bracketSize.list;
+  type: tournamentType;
   gameName: string;
+  bracketSizeType: bracketSize;
+  participantNumber: number;
   has3rdPlaceGame?: boolean;
-  howPlayEachOther?: howManyTimes = howManyTimes.One;
-  participantNames?: [];
+  howPlayEachOther?: howManyGames;
+  participantNames?: string[];
 
   constructor(
     name: string,
     type: tournamentType,
-    participantNumber: bracketSize,
     gameName: string,
+    bracketSizeType: bracketSize,
+    participantNumber: number,
     has3rdPlaceGame?: boolean,
-    howPlayEachOther?: howManyTimes,
-    participantNames?: []
+    howPlayEachOther?: howManyGames,
+    participantNames?: string[]
   ) {
     this.name = name;
     this.type = type;
-    this.participantNumber = participantNumber;
     this.gameName = gameName;
+    this.bracketSizeType = bracketSizeType;
+    this.participantNumber = participantNumber;
     this.has3rdPlaceGame = has3rdPlaceGame;
     this.howPlayEachOther = howPlayEachOther;
     this.participantNames = participantNames;
